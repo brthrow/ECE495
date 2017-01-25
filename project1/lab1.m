339c7e95b713dba34458d2a3febf989d456bd6a2
jms6 <jms6@clemson.edu>
2017-01-25 13:38:46 -0500

    folder created, finalized matlab, figure and block diagram bitmaps

---
+++ project1/lab1.m
@@ -0,0 +1,33 @@
+clc
+close all
+
+decimation = 1;
+samples = 250;
+timestep = 0.0001;
+frequency = 2000;
+rad = 12566;
+
+figure(1)
+p1 = plot(tg.TimeLog, tg.OutputLog(:,1));
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('2000 Hz Sinusoidal Wave');
+grid on
+axis([1,1.01,-1.1,1.1]);
+%{
+figure(2)
+p2 = plot(tg.TimeLog, tg.OutputLog(:,2));
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('100 Hz Square Wave');
+grid on
+axis([0,0.1,-1,6]);
+
+figure(3)
+p3 = plot(tg.TimeLog, tg.OutputLog(:,3));
+xlabel('Time (s)');
+ylabel('Count (#)');
+title('Encoder');
+grid on
+%axis([0,3.5,-40,100]);
+%}
