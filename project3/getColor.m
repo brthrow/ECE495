3d4bec73c122a40b013228be09ff5b0ede801cdf
Blake Thrower <brthrow@g.clemson.edu>
2017-03-15 15:44:21 -0400

    Updated color thresholding

--- project3/getColor.m
+++ project3/getColor.m
@@ -1,16 +1,16 @@
 function c = getColor(r, g, b)
 %   Detailed explanation goes here
-    if (r > 60) && (r < 105) && (g > 55) && (g < 95) && (b > 100) && (b < 185)
+    if (r > 60) && (r < 105) && (g > 100) && (g < 135) && (b > 165) && (b < 200)
         c = 'blue';
-    elseif (r < 100) && (g < 75) && (b < 100)
+    elseif (r < 105) && (g < 100) && (b < 100)
         c = 'black';
-    elseif (r > 240) && (g > 100) && (g < 200) && (b > 140)
+    elseif (r > 235) && (g > 150) && (g < 205) && (b > 120) && (b < 175)
         c = 'orange';
-    elseif (r > 240) && (g < 100) && (b > 100) && (b < 200)
+    elseif (r > 220) && (g < 75) && (b > 150) && (b < 170)
         c = 'red';
     elseif (r > 240) && (g > 240) && (b > 240)
         c = 'white';
-    elseif (r > 240) && (g > 200) && (b > 100) && (b < 200)
+    elseif (r > 240) && (g > 225) && (b > 115) && (b < 175)
         c = 'yellow';
     else
         c = 'undetermined';
